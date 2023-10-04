1918805766b6f6c6f0aa56120ed1739da1938517
Shamash <redboot1@gmail.com>
2023-10-04 23:06:16 +0300

    npm run build change view main js

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-console.log({leatherStrips:2,ironIngot:1,refinedMoonstone:4});
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const elvenShieldRecipe = {\r\n    leatherStrips: 2,\r\n    ironIngot: 1,\r\n    refinedMoonstone: 4\r\n}\r\n\r\n\r\nconst elvenGauntletsRecipe = {\r\n    ...elvenShieldRecipe, \r\n    leather: 1, \r\n    refinedMoonstone: 4,\r\n}\r\n\r\nconsole.log(elvenShiledRecipe);\r\nconsole.log(elvenGauntletsRecipe);\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
